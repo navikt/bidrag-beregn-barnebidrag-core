5e1afed8a177745e695bd988afa0a5691b42c154
rinnan17 <magnus.rinnan@nav.no>
2020-08-31 12:34:31 +0200

    Beregning av kostnadsberegnet bidrag ferdig

--- src/main/java/no/nav/bidrag/beregn/kostnadsberegnetbidrag/periode/KostnadsberegnetBidragPeriodeImpl.java
+++ src/main/java/no/nav/bidrag/beregn/kostnadsberegnetbidrag/periode/KostnadsberegnetBidragPeriodeImpl.java
@@ -2,12 +2,12 @@ package no.nav.bidrag.beregn.kostnadsberegnetbidrag.periode;
 
 import static java.util.stream.Collectors.toCollection;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import no.nav.bidrag.beregn.bpsandelunderholdskostnad.bo.BeregnBPsAndelUnderholdskostnadGrunnlag;
+import no.nav.bidrag.beregn.felles.PeriodeUtil;
 import no.nav.bidrag.beregn.felles.bo.Avvik;
 import no.nav.bidrag.beregn.felles.bo.Periode;
-import no.nav.bidrag.beregn.felles.enums.AvvikType;
 import no.nav.bidrag.beregn.felles.periode.Periodiserer;
 import no.nav.bidrag.beregn.kostnadsberegnetbidrag.beregning.KostnadsberegnetBidragBeregning;
 import no.nav.bidrag.beregn.kostnadsberegnetbidrag.bo.BPsAndelUnderholdskostnadPeriode;
@@ -96,110 +96,43 @@ public class KostnadsberegnetBidragPeriodeImpl implements KostnadsberegnetBidrag
     return new BeregnKostnadsberegnetBidragResultat(resultatPeriodeListe);
   }
 
-  // Validerer at input-verdier til underholdskostnadsberegning er gyldige
-  public List<Avvik> validerInput(
-      BeregnKostnadsberegnetBidragGrunnlag beregnKostnadsberegnetBidragGrunnlag) {
-    var avvikListe = new ArrayList<Avvik>();
+  // Validerer at input-verdier til kostnadsberegnet bidrag er gyldige
+  public List<Avvik> validerInput(BeregnKostnadsberegnetBidragGrunnlag grunnlag) {
 
-    // Sjekk perioder for underholdskostnad
+    // Sjekk perioder for sjablonliste
     var underholdskostnadPeriodeListe = new ArrayList<Periode>();
-    for (UnderholdskostnadPeriode underholdskostnadPeriode : beregnKostnadsberegnetBidragGrunnlag.getUnderholdskostnadPeriodeListe()) {
+    for (UnderholdskostnadPeriode underholdskostnadPeriode : grunnlag
+        .getUnderholdskostnadPeriodeListe()) {
       underholdskostnadPeriodeListe.add(underholdskostnadPeriode.getDatoFraTil());
     }
-    avvikListe.addAll(validerInput("underholdskostnadListe", underholdskostnadPeriodeListe, true, true, true));
+    var avvikListe = new ArrayList<>(
+        PeriodeUtil.validerInputDatoer(grunnlag.getBeregnDatoFra(), grunnlag.getBeregnDatoTil(),
+            "underholdskostnadPeriodeListe",
+            underholdskostnadPeriodeListe, true, true, false, true));
 
-    // Sjekk perioder for BPs andel av underholdskostnad
+    // Sjekk perioder for bps andel av underholdskostnad
     var bPsAndelUnderholdskostnadPeriodeListe = new ArrayList<Periode>();
-    for (BPsAndelUnderholdskostnadPeriode bPsAndelUnderholdskostnadPeriode : beregnKostnadsberegnetBidragGrunnlag
+    for (BPsAndelUnderholdskostnadPeriode bPsAndelUnderholdskostnadPeriode : grunnlag
         .getBPsAndelUnderholdskostnadPeriodeListe()) {
       bPsAndelUnderholdskostnadPeriodeListe.add(bPsAndelUnderholdskostnadPeriode.getDatoFraTil());
     }
-    avvikListe.addAll(validerInput("bPsAndelUnderholdskostnadPeriodeListe", bPsAndelUnderholdskostnadPeriodeListe, true, true, true));
+    avvikListe.addAll(
+        PeriodeUtil.validerInputDatoer(grunnlag.getBeregnDatoFra(), grunnlag.getBeregnDatoTil(),
+            "bPsAndelUnderholdskostnadPeriodeListe",
+            bPsAndelUnderholdskostnadPeriodeListe, true, true, false, true));
 
     // Sjekk perioder for samværsfradrag
     var samvaersfradragPeriodeListe = new ArrayList<Periode>();
-    for (SamvaersfradragPeriode samvaersfradragPeriode : beregnKostnadsberegnetBidragGrunnlag.getSamvaersfradragPeriodeListe()) {
-      samvaersfradragPeriodeListe.add(samvaersfradragPeriode.getDatoFraTil());
+    for (SamvaersfradragPeriode samvaersfradragPeriode : grunnlag
+        .getSamvaersfradragPeriodeListe()) {
+      bPsAndelUnderholdskostnadPeriodeListe.add(samvaersfradragPeriode.getDatoFraTil());
     }
-    avvikListe.addAll(validerInput("samvaersfradragPeriodeListe", samvaersfradragPeriodeListe, true, true, true));
-
-    // Sjekk beregn dato fra/til
-    avvikListe.addAll(validerBeregnPeriodeInput(beregnKostnadsberegnetBidragGrunnlag.getBeregnDatoFra(), beregnKostnadsberegnetBidragGrunnlag
-        .getBeregnDatoTil()));
+    avvikListe.addAll(
+        PeriodeUtil.validerInputDatoer(grunnlag.getBeregnDatoFra(), grunnlag.getBeregnDatoTil(),
+            "samværsfradragPeriodeListe",
+            samvaersfradragPeriodeListe, false, false, false, false));
 
     return avvikListe;
   }
 
-  // Validerer at datoer er gyldige
-  private List<Avvik> validerInput(String dataElement, List<Periode> periodeListe, boolean sjekkOverlapp, boolean sjekkOpphold, boolean sjekkNull) {
-    var avvikListe = new ArrayList<Avvik>();
-    int indeks = 0;
-    Periode forrigePeriode = null;
-
-    for (Periode dennePeriode : periodeListe) {
-      indeks++;
-
-      //Sjekk om perioder overlapper
-      if (sjekkOverlapp) {
-        if (dennePeriode.overlapper(forrigePeriode)) {
-          var feilmelding = "Overlappende perioder i " + dataElement + ": periodeDatoTil=" + forrigePeriode.getDatoTil() + ", periodeDatoFra=" +
-              dennePeriode.getDatoFra();
-          avvikListe.add(new Avvik(feilmelding, AvvikType.PERIODER_OVERLAPPER));
-        }
-      }
-
-      //Sjekk om det er opphold mellom perioder
-      if (sjekkOpphold) {
-        if (dennePeriode.harOpphold(forrigePeriode)) {
-          var feilmelding = "Opphold mellom perioder i " + dataElement + ": periodeDatoTil=" + forrigePeriode.getDatoTil() + ", periodeDatoFra=" +
-              dennePeriode.getDatoFra();
-          avvikListe.add(new Avvik(feilmelding, AvvikType.PERIODER_HAR_OPPHOLD));
-        }
-      }
-
-      //Sjekk om dato er null
-      if (sjekkNull) {
-        if ((indeks != periodeListe.size()) && (dennePeriode.getDatoTil() == null)) {
-          var feilmelding = "periodeDatoTil kan ikke være null i " + dataElement + ": periodeDatoFra=" + dennePeriode.getDatoFra() +
-              ", periodeDatoTil=" + dennePeriode.getDatoTil();
-          avvikListe.add(new Avvik(feilmelding, AvvikType.NULL_VERDI_I_DATO));
-        }
-        if ((indeks != 1) && (dennePeriode.getDatoFra() == null)) {
-          var feilmelding = "periodeDatoFra kan ikke være null i " + dataElement + ": periodeDatoFra=" + dennePeriode.getDatoFra() +
-              ", periodeDatoTil=" + dennePeriode.getDatoTil();
-          avvikListe.add(new Avvik(feilmelding, AvvikType.NULL_VERDI_I_DATO));
-        }
-      }
-
-      //Sjekk om dato fra er etter dato til
-      if (!(dennePeriode.datoTilErEtterDatoFra())) {
-        var feilmelding = "periodeDatoTil må være etter periodeDatoFra i " + dataElement + ": periodeDatoFra=" + dennePeriode.getDatoFra() +
-            ", periodeDatoTil=" + dennePeriode.getDatoTil();
-        avvikListe.add(new Avvik(feilmelding, AvvikType.DATO_FRA_ETTER_DATO_TIL));
-      }
-
-      forrigePeriode = new Periode(dennePeriode.getDatoFra(), dennePeriode.getDatoTil());
-    }
-
-    return avvikListe;
-  }
-
-  // Validerer at beregningsperiode fra/til er gyldig
-  private List<Avvik> validerBeregnPeriodeInput(LocalDate beregnDatoFra, LocalDate beregnDatoTil) {
-    var avvikListe = new ArrayList<Avvik>();
-
-    if (beregnDatoFra == null) {
-      avvikListe.add(new Avvik("beregnDatoFra kan ikke være null", AvvikType.NULL_VERDI_I_DATO));
-    }
-    if (beregnDatoTil == null) {
-      avvikListe.add(new Avvik("beregnDatoTil kan ikke være null", AvvikType.NULL_VERDI_I_DATO));
-    }
-    if (!new Periode(beregnDatoFra, beregnDatoTil).datoTilErEtterDatoFra()) {
-      avvikListe.add(new Avvik("beregnDatoTil må være etter beregnDatoFra", AvvikType.DATO_FRA_ETTER_DATO_TIL));
-    }
-
-    return avvikListe;
-  }
 }
-
-
