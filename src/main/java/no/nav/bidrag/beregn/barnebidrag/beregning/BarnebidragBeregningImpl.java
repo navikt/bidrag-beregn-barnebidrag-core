6cbee72eed202334350bd82e5f2f7e479aea8bf3
rinnan17 <magnus.rinnan@nav.no>
2020-09-16 07:58:16 +0200

    midaircommit barnebidrag

--- src/main/java/no/nav/bidrag/beregn/barnebidrag/beregning/BarnebidragBeregningImpl.java
+++ src/main/java/no/nav/bidrag/beregn/barnebidrag/beregning/BarnebidragBeregningImpl.java
@@ -1,15 +1,9 @@
 package no.nav.bidrag.beregn.barnebidrag.beregning;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-import no.nav.bidrag.beregn.felles.SjablonUtil;
 import no.nav.bidrag.beregn.felles.bo.SjablonNokkel;
 import no.nav.bidrag.beregn.felles.enums.ResultatKode;
-import no.nav.bidrag.beregn.felles.enums.SjablonInnholdNavn;
-import no.nav.bidrag.beregn.felles.enums.SjablonNavn;
-import no.nav.bidrag.beregn.felles.enums.SjablonNokkelNavn;
 import no.nav.bidrag.beregn.barnebidrag.bo.GrunnlagBeregningPeriodisert;
 import no.nav.bidrag.beregn.barnebidrag.bo.ResultatBeregning;
 
